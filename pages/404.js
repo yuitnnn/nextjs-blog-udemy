287be2bf3e988dc685a1bc91efb16a1becc3e9fd
yuikoro12@gmail.com <yuikoro12@gmail.com>
2023-08-11 22:50:44 +0900

    first commit

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <h1>
+      ページが見つかりませんでした。
+    </h1>
+  );
+}
---
+++ pages/posts/firstPost.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+	return <div>
+		<h1>最初の投稿</h1>
+		<Link href="/">ホームへ戻る</Link>
+  </div>
+}
